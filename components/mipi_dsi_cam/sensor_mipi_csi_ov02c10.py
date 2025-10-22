a4a61482c6e90472d78c2e0faa5c638a31903f2e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-22 06:26:42 +0200

    Update sensor_mipi_csi_ov02c10.py

--- components/mipi_dsi_cam/sensor_mipi_csi_ov02c10.py
+++ components/mipi_dsi_cam/sensor_mipi_csi_ov02c10.py
@@ -3,7 +3,7 @@ SENSOR_INFO = {
     'manufacturer': 'OmniVision',
     'pid': 0x5602,
     'i2c_address': 0x36,
-    'lane_count': 1,  # 1 lane pour le capteur original
+    'lane_count': 2,  # 1 lane pour le capteur original
     'bayer_pattern': 1,  # GBRG (ESP_CAM_SENSOR_BAYER_GBRG)
     'lane_bitrate_mbps': 400,  # 100MHz * 4 = 400 Mbps
     'width': 1288,
