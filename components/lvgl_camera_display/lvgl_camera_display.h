7d140fa1b0bd061c55a5e245a0aeb6c2d9ff40e8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-22 18:11:22 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -7,7 +7,7 @@
 #ifdef USE_ESP32_VARIANT_ESP32P4
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <sys/mman.h>
+#include "mman.h"
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
 #include "driver/ppa.h"
