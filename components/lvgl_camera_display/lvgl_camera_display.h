7943e10c7f205f8bd6c90d9ee631170c617eb262
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-24 19:01:55 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -1,28 +1,3 @@
-#pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/components/lvgl/lvgl_esphome.h"
-
-#ifdef USE_ESP32_VARIANT_ESP32P4
-#include <fcntl.h>
-#include <sys/ioctl.h>
-//#include "ioctl.h"
-#include "mman.h"
-#include "../mipi_dsi_cam/videodev2.h"
-#include "../mipi_dsi_cam/esp_video_device.h"
-#include "../mipi_dsi_cam/mipi_dsi_cam.h"
-#include "../mipi_dsi_cam/videodev2.h"
-
-#include "driver/ppa.h"
-#include "esp_cache.h"
-
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#endif
-
 #include <cstddef>
 #include <cstdint>
 
@@ -134,3 +109,4 @@ class LVGLCameraDisplay : public Component {
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
+
