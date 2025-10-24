b718e79506115bec1bd59dba5642af3317c2b1c8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-25 00:43:56 +0200

    Update esp_video_init.h

--- components/mipi_dsi_cam/esp_video_init.h
+++ components/mipi_dsi_cam/esp_video_init.h
@@ -9,98 +9,97 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "esp_err.h"
-#include "driver/i2c_master.h"
-#include "esp_cam_ctlr_dvp.h"
-#include "driver/jpeg_encode.h"
+
+#ifdef __has_include
+  #if __has_include("driver/i2c_master.h")
+    #include "driver/i2c_master.h"
+  #else
+    typedef void *i2c_master_bus_handle_t;
+  #endif
+
+  #if __has_include("esp_cam_ctlr_dvp.h")
+    #include "esp_cam_ctlr_dvp.h"
+  #else
+    typedef struct {
+        int pclk_io;
+        int vsync_io;
+        int href_io;
+        int data_io[8];
+        int xclk_io;
+    } esp_cam_ctlr_dvp_pin_config_t;
+  #endif
+
+  #if __has_include("driver/jpeg_encode.h")
+    #include "driver/jpeg_encode.h"
+  #else
+    typedef void *jpeg_encoder_handle_t;
+  #endif
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/**
- * @brief SCCB initialization configuration
- */
+/** SCCB initialization configuration **/
 typedef struct esp_video_init_sccb_config {
-    bool init_sccb; /*!< true:  SCCB I2C is not initialized and esp_video_init function will initialize SCCB I2C with
-                       given parameters i2c_config. false: SCCB I2C is initialized and esp_video_init function can use
-                       i2c_handle directly */
+    bool init_sccb;
     union {
         struct {
-            uint8_t port;    /*!< SCCB I2C port */
-            uint8_t scl_pin; /*!< SCCB I2C SCL pin */
-            uint8_t sda_pin; /*!< SCCB I2C SDA pin */
+            uint8_t port;
+            uint8_t scl_pin;
+            uint8_t sda_pin;
         } i2c_config;
-
-        i2c_master_bus_handle_t i2c_handle; /*!< SCCB I2C handle */
+        i2c_master_bus_handle_t i2c_handle;
     };
-
-    uint32_t freq; /*!< SCCB I2C frequency */
+    uint32_t freq;
 } esp_video_init_sccb_config_t;
 
-/**
- * @brief MIPI CSI initialization and camera sensor connection configuration
- */
+/** MIPI CSI configuration **/
 typedef struct esp_video_init_csi_config {
-    esp_video_init_sccb_config_t sccb_config; /*!< Camera sensor SCCB configuration */
-
-    int8_t reset_pin; /*!< Camera sensor reset pin, if hardware has no reset pin, set reset_pin to be -1 */
-    int8_t pwdn_pin;  /*!< Camera sensor power down pin, if hardware has no power down pin, set pwdn_pin to be -1 */
+    esp_video_init_sccb_config_t sccb_config;
+    int8_t reset_pin;
+    int8_t pwdn_pin;
 } esp_video_init_csi_config_t;
 
-/**
- * @brief DVP initialization and camera sensor connection configuration
- */
+/** DVP configuration **/
 typedef struct esp_video_init_dvp_config {
-    esp_video_init_sccb_config_t sccb_config; /*!< Camera sensor SCCB configuration */
-
-    int8_t reset_pin; /*!< Camera sensor reset pin, if hardware has no reset pin, set reset_pin to be -1 */
-    int8_t pwdn_pin;  /*!< Camera sensor power down pin, if hardware has no power down pin, set pwdn_pin to be -1 */
-
-    esp_cam_ctlr_dvp_pin_config_t dvp_pin; /*!< DVP pin configuration */
-
-    uint32_t xclk_freq; /*!< DVP hardware output clock frequency */
+    esp_video_init_sccb_config_t sccb_config;
+    int8_t reset_pin;
+    int8_t pwdn_pin;
+    esp_cam_ctlr_dvp_pin_config_t dvp_pin;
+    uint32_t xclk_freq;
 } esp_video_init_dvp_config_t;
 
-/**
- * @brief JPEG initialization configuration
- */
+/** JPEG configuration **/
 typedef struct esp_video_init_jpeg_config {
-    jpeg_encoder_handle_t enc_handle; /*!< JPEG encoder driver handle:
-                                           - NULL, JPEG video device will create JPEG encoder driver handle by itself
-                                           - Not null, JPEG video device will use this handle instead of creating JPEG
-                                         encoder driver handle */
+    jpeg_encoder_handle_t enc_handle;
 } esp_video_init_jpeg_config_t;
 
-/**
- * @brief ISP initialization configuration
- */
+/** ISP configuration **/
 typedef struct esp_video_init_isp_config {
-    int ipa_nums;           /*!< Image process algorithm numbers */
-    const char **ipa_names; /*!< Image process algorithm name array */
+    int ipa_nums;
+    const char **ipa_names;
 } esp_video_init_isp_config_t;
 
-/**
- * @brief Video hardware initialization configuration
- */
+/** Global initialization configuration **/
 typedef struct esp_video_init_config {
-    const esp_video_init_csi_config_t *csi;   /*!< MIPI CSI initialization configuration */
-    const esp_video_init_dvp_config_t *dvp;   /*!< DVP initialization configuration array */
-    const esp_video_init_jpeg_config_t *jpeg; /*!< JPEG initialization configuration */
-    const esp_video_init_isp_config_t
-        *isp; /*!< ISP initialization configuration, using default config if setting NULL */
+    const esp_video_init_csi_config_t *csi;
+    const esp_video_init_dvp_config_t *dvp;
+    const esp_video_init_jpeg_config_t *jpeg;
+    const esp_video_init_isp_config_t *isp;
 } esp_video_init_config_t;
 
 /**
- * @brief Initialize video hardware and software, including I2C, MIPI CSI and so on.
- *
- * @param config video hardware configuration
- *
- * @return
- *      - ESP_OK on success
- *      - Others if failed
+ * @brief Initialize video hardware and software (I2C, MIPI CSI, etc.)
  */
 esp_err_t esp_video_init(const esp_video_init_config_t *config);
+
+/**
+ * @brief Deinitialize and free video hardware resources.
+ */
+esp_err_t esp_video_deinit(void);
 
 #ifdef __cplusplus
 }
 #endif
+
