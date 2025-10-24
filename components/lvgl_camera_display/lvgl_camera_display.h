e4fb7b6a75b42b2818032b9707acea1e8ef572df
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-25 01:00:40 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -11,7 +11,7 @@
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
-#include "esp_video_init.h"        // <- pour esp_video_init()/deinit()
+#include "../mipi_dsi_cam/esp_video_init.h"        // <- pour esp_video_init()/deinit()
 #include "driver/ppa.h"
 #include "esp_cache.h"
 #endif
