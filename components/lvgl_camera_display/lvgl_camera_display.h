cecaa0a5fe83c103e3aef056526853924ecca1db
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-24 23:24:53 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -7,7 +7,7 @@
 #ifdef USE_ESP32_VARIANT_ESP32P4
 #include <fcntl.h>
 #include <sys/ioctl.h>
-//#include "ioctl.h"
+#include "ioctl.h"
 #include "mman.h"
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
@@ -15,13 +15,6 @@
 #include "driver/ppa.h"
 #include "esp_cache.h"
 #endif
-
-namespace mipi_dsi_cam {
-class MipiDsiCam;
-}
-
-#include <cstddef>
-#include <cstdint>
 
 namespace esphome {
 namespace lvgl_camera_display {
@@ -35,30 +28,7 @@ enum Rotation {
   ROTATION_90 = 90,
   ROTATION_180 = 180,
   ROTATION_270 = 270
-static constexpr size_t kVideoBufferCount = 3;  // Triple buffering
-
-#ifdef ESP_VIDEO_MIPI_CSI_DEVICE_NAME
-constexpr const char *kDefaultVideoDevice = ESP_VIDEO_MIPI_CSI_DEVICE_NAME;
-#else
-constexpr const char *kDefaultVideoDevice = "/dev/video0";
-#endif
-
-enum class Rotation : uint16_t {
-  DEG_0 = 0,
-  DEG_90 = 90,
-  DEG_180 = 180,
-  DEG_270 = 270,
 };
-
-// Backwards compatible aliases for the previous enumerator-style constants.
-constexpr Rotation ROTATION_0 = Rotation::DEG_0;
-constexpr Rotation ROTATION_90 = Rotation::DEG_90;
-constexpr Rotation ROTATION_180 = Rotation::DEG_180;
-constexpr Rotation ROTATION_270 = Rotation::DEG_270;
-
-constexpr uint16_t to_degrees(Rotation rotation) {
-  return static_cast<uint16_t>(rotation);
-}
 
 class LVGLCameraDisplay : public Component {
  public:
@@ -75,13 +45,11 @@ class LVGLCameraDisplay : public Component {
   void configure_canvas(lv_obj_t *canvas);
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
   
-
   // Transformations PPA
   void set_rotation(Rotation rotation) { this->rotation_ = rotation; }
   void set_mirror_x(bool enable) { this->mirror_x_ = enable; }
   void set_mirror_y(bool enable) { this->mirror_y_ = enable; }
   
-
   // Stats
   uint32_t get_frame_count() const { return this->frame_count_; }
 
@@ -90,38 +58,25 @@ class LVGLCameraDisplay : public Component {
   
 #ifdef USE_ESP32_VARIANT_ESP32P4
   // V4L2 device
-
-  // V4L2 device (ESP32-P4 only)
   int video_fd_{-1};
   const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};  // "/dev/video0"
   
-  const char *video_device_ = kDefaultVideoDevice;
-
   // Buffers mmappés (zero-copy)
   uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT]{nullptr};
   size_t buffer_length_{0};
   
-  uint8_t *mmap_buffers_[kVideoBufferCount] = {nullptr};
-  size_t buffer_lengths_[kVideoBufferCount] = {0};
-  size_t buffer_count_{0};
-
   // PPA pour transformations
-#ifdef USE_ESP32_VARIANT_ESP32P4
   ppa_client_handle_t ppa_handle_{nullptr};
   uint8_t *transform_buffer_{nullptr};
   size_t transform_buffer_size_{0};
   
-
   // Méthodes V4L2
   bool open_video_device_();
   bool setup_buffers_();
   bool start_streaming_();
   bool capture_frame_();
-  bool queue_buffer_(uint32_t index);
-  bool ensure_camera_streaming_();
   void cleanup_();
   
-
   // PPA
   bool init_ppa_();
   void deinit_ppa_();
@@ -131,27 +86,22 @@ class LVGLCameraDisplay : public Component {
   // Canvas LVGL
   lv_obj_t *canvas_obj_{nullptr};
   
-
   // Configuration
   uint16_t width_{1280};
   uint16_t height_{720};
   Rotation rotation_{ROTATION_0};
-  Rotation rotation_ = Rotation::DEG_0;
   bool mirror_x_{false};
   bool mirror_y_{false};
   uint32_t update_interval_{33};  // ~30 FPS
   
-
   // Stats
   uint32_t frame_count_{0};
   uint32_t last_update_time_{0};
   uint32_t last_fps_time_{0};
   
-
   // Flags
   bool first_update_{true};
   bool streaming_{false};
-  bool camera_streaming_started_{false};
   uint8_t *last_display_buffer_{nullptr};
 };
 
