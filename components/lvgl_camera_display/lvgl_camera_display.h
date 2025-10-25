ffb3842f310990c454eb23b5ebc4ec9f35e2eea0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-25 02:23:14 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -5,98 +5,108 @@
 #include "esphome/components/lvgl/lvgl_esphome.h"
 
 #ifdef USE_ESP32_VARIANT_ESP32P4
-
 #include <fcntl.h>
-#include "ioctl.h"   // headers locaux
+#include <sys/ioctl.h>
+#include "ioctl.h"
 #include "mman.h"
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
 #include "driver/ppa.h"
 #include "esp_cache.h"
-#include <sys/types.h>
-
-#endif  // USE_ESP32_VARIANT_ESP32P4
+#endif
 
 namespace esphome {
 namespace lvgl_camera_display {
 
-#define VIDEO_BUFFER_COUNT 3
+// Configuration
+#define VIDEO_BUFFER_COUNT 3  // Triple buffering
+#define MEMORY_TYPE V4L2_MEMORY_MMAP
 
-class LVGLCameraDisplay : public esphome::Component {
+enum Rotation {
+  ROTATION_0 = 0,
+  ROTATION_90 = 90,
+  ROTATION_180 = 180,
+  ROTATION_270 = 270
+};
+
+class LVGLCameraDisplay : public Component {
  public:
-  enum Rotation {
-    ROTATION_0 = 0,
-    ROTATION_90 = 90,
-    ROTATION_180 = 180,
-    ROTATION_270 = 270
-  };
-
-  // --- Méthodes principales ---
   void setup() override;
   void loop() override;
   void dump_config() override;
-  void configure_canvas(lv_obj_t *canvas);
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
-  // --- Configuration dynamique ---
-  void set_camera(esphome::mipi_dsi_cam::MipiDsiCam *cam) { this->camera_ = cam; }
+  // Configuration
+  void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
+  void set_video_device(const char *dev) { this->video_device_ = dev; }
+  void set_width(uint16_t width) { this->width_ = width; }
+  void set_height(uint16_t height) { this->height_ = height; }
+  void configure_canvas(lv_obj_t *canvas);
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
-  void set_rotation(Rotation rot) { this->rotation_ = rot; }
-  void set_mirror_x(bool enabled) { this->mirror_x_ = enabled; }
-  void set_mirror_y(bool enabled) { this->mirror_y_ = enabled; }
+  
+  // Transformations PPA
+  void set_rotation(Rotation rotation) { this->rotation_ = rotation; }
+  void set_mirror_x(bool enable) { this->mirror_x_ = enable; }
+  void set_mirror_y(bool enable) { this->mirror_y_ = enable; }
+  
+  // Stats
+  uint32_t get_frame_count() const { return this->frame_count_; }
 
  protected:
+  mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
+  
 #ifdef USE_ESP32_VARIANT_ESP32P4
-  // --- Caméra et LVGL ---
-  esphome::mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
-  lv_obj_t *canvas_obj_{nullptr};
-
-  // --- Device vidéo V4L2 ---
+  // V4L2 device
   int video_fd_{-1};
-  const char *video_device_ = ESP_VIDEO_MIPI_CSI_DEVICE_NAME;
-
-  // --- Résolution ---
-  uint32_t width_{0};
-  uint32_t height_{0};
-
-  // --- Buffers mmap ---
-  uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT] = {nullptr};
+  const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};  // "/dev/video0"
+  
+  // Buffers mmappés (zero-copy)
+  uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT]{nullptr};
   size_t buffer_length_{0};
-  bool streaming_{false};
-
-  // --- PPA matériel ---
+  
+  // PPA pour transformations
   ppa_client_handle_t ppa_handle_{nullptr};
   uint8_t *transform_buffer_{nullptr};
   size_t transform_buffer_size_{0};
-
-  // --- Options logiques ---
-  Rotation rotation_{ROTATION_0};
-  bool mirror_x_{false};
-  bool mirror_y_{false};
-  uint32_t update_interval_{33};
-
-  // --- Statistiques ---
-  uint32_t last_update_time_{0};
-  uint32_t frame_count_{0};
-  uint32_t last_fps_time_{0};
-
-  // --- Méthodes internes ---
+  
+  // Méthodes V4L2
   bool open_video_device_();
-  bool negotiate_format_();
   bool setup_buffers_();
   bool start_streaming_();
   bool capture_frame_();
   void cleanup_();
-
+  
+  // PPA
   bool init_ppa_();
   void deinit_ppa_();
   bool transform_frame_(const uint8_t *src, uint8_t *dst);
 #endif
+
+  // Canvas LVGL
+  lv_obj_t *canvas_obj_{nullptr};
+  
+  // Configuration
+  uint16_t width_{1280};
+  uint16_t height_{720};
+  Rotation rotation_{ROTATION_0};
+  bool mirror_x_{false};
+  bool mirror_y_{false};
+  uint32_t update_interval_{33};  // ~30 FPS
+  
+  // Stats
+  uint32_t frame_count_{0};
+  uint32_t last_update_time_{0};
+  uint32_t last_fps_time_{0};
+  
+  // Flags
+  bool first_update_{true};
+  bool streaming_{false};
+  uint8_t *last_display_buffer_{nullptr};
 };
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
-
 
 
 
