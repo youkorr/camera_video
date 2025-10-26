e2b9e0e0faea0467d94fa30252f012ca0df242d0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-26 14:24:12 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -7,8 +7,7 @@
 #ifdef USE_ESP32_VARIANT_ESP32P4
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include "ioctl.h"
-#include "mman.h"
+#include <sys/mman.h>
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
 #include "../mipi_dsi_cam/esp_video_init.h"
@@ -21,10 +20,10 @@ namespace esphome {
 namespace lvgl_camera_display {
 
 // Configuration
-#define VIDEO_BUFFER_COUNT 2  // Triple buffering
+#define VIDEO_BUFFER_COUNT 2
 #define MEMORY_TYPE V4L2_MEMORY_MMAP
 
-enum Rotation {
+enum RotationAngle {
   ROTATION_0 = 0,
   ROTATION_90 = 90,
   ROTATION_180 = 180,
@@ -40,14 +39,13 @@ class LVGLCameraDisplay : public Component {
 
   // Configuration
   void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
-  void set_video_device(const char *dev) { this->video_device_ = dev; }
   void set_width(uint16_t width) { this->width_ = width; }
   void set_height(uint16_t height) { this->height_ = height; }
   void configure_canvas(lv_obj_t *canvas);
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
   
   // Transformations PPA
-  void set_rotation(Rotation rotation) { this->rotation_ = rotation; }
+  void set_rotation(RotationAngle rotation) { this->rotation_ = rotation; }
   void set_mirror_x(bool enable) { this->mirror_x_ = enable; }
   void set_mirror_y(bool enable) { this->mirror_y_ = enable; }
   
@@ -60,9 +58,9 @@ class LVGLCameraDisplay : public Component {
 #ifdef USE_ESP32_VARIANT_ESP32P4
   // V4L2 device
   int video_fd_{-1};
-  const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};  // "/dev/video0"
+  const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};
   
-  // Buffers mmappés (zero-copy)
+  // Buffers mmappés
   uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT]{nullptr};
   size_t buffer_length_{0};
   
@@ -72,11 +70,12 @@ class LVGLCameraDisplay : public Component {
   size_t transform_buffer_size_{0};
   
   // Méthodes V4L2
-  bool open_video_device_();
-  bool setup_buffers_();
-  bool start_streaming_();
-  bool capture_frame_();
-  void cleanup_();
+  bool init_v4l2_device_();
+  bool setup_v4l2_format_();
+  bool setup_v4l2_buffers_();
+  bool start_v4l2_streaming_();
+  bool capture_v4l2_frame_(uint8_t **frame_data);
+  void cleanup_v4l2_();
   
   // PPA
   bool init_ppa_();
@@ -90,19 +89,20 @@ class LVGLCameraDisplay : public Component {
   // Configuration
   uint16_t width_{1280};
   uint16_t height_{720};
-  Rotation rotation_{ROTATION_0};
+  RotationAngle rotation_{ROTATION_0};
   bool mirror_x_{false};
   bool mirror_y_{false};
-  uint32_t update_interval_{33};  // ~30 FPS
+  uint32_t update_interval_{33};
   
   // Stats
   uint32_t frame_count_{0};
+  uint32_t drop_count_{0};
   uint32_t last_update_time_{0};
   uint32_t last_fps_time_{0};
   
   // Flags
   bool first_update_{true};
-  bool streaming_{false};
+  bool v4l2_streaming_{false};
   uint8_t *last_display_buffer_{nullptr};
 };
 
