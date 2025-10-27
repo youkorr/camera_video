ad44a0ec5eef599dc4f193d4fbab53d9000b22ed
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-27 05:08:19 +0100

    Update mipi_dsi_cam_v4l2_adapter.h

--- components/mipi_dsi_cam/mipi_dsi_cam_v4l2_adapter.h
+++ components/mipi_dsi_cam/mipi_dsi_cam_v4l2_adapter.h
@@ -10,7 +10,7 @@ extern "C" {
 }
 
 // Déclaration de la fonction d'enregistrement du device
-extern "C" esp_err_t esp_video_register_device(int device_id, void *video_device, 
+extern "C" esp_err_t esp_video_register_device(int device_id, void *video_device,
                                                 void *user_ctx, const void *ops);
 
 namespace esphome {
@@ -20,23 +20,26 @@ namespace mipi_dsi_cam {
 struct MipiCameraV4L2Context {
   MipiDsiCam *camera;
   void *video_device;
-  
+
   // Configuration du format
   uint32_t width;
   uint32_t height;
   uint32_t pixelformat;
-  
+
   // État du streaming
   bool streaming;
-  
+
   // Gestion des buffers
   struct esp_video_buffer *buffers;
   uint32_t buffer_count;
   uint32_t queued_count;
-  
+
   // Statistiques
   uint32_t frame_count;
   uint32_t drop_count;
+
+  // Suivi des frames pour éviter les doublons entre consommateurs
+  uint32_t last_frame_sequence;
 };
 
 // Structure pour les opérations V4L2
@@ -57,43 +60,43 @@ struct esp_video_ops {
 
 /**
  * @brief Adaptateur V4L2 pour mipi_dsi_cam
- * 
+ *
  * Implémentation complète de l'interface V4L2 standard
  */
 class MipiDsiCamV4L2Adapter {
  public:
   MipiDsiCamV4L2Adapter(MipiDsiCam *camera);
   ~MipiDsiCamV4L2Adapter();
-  
+
   /**
    * @brief Initialise l'interface V4L2
    * @return ESP_OK si réussi
    */
   esp_err_t init();
-  
+
   /**
    * @brief Libère les ressources V4L2
    * @return ESP_OK si réussi
    */
   esp_err_t deinit();
-  
+
   /**
    * @brief Retourne le pointeur vers le device V4L2
    */
   void* get_video_device() { return &this->context_; }
-  
+
   /**
    * @brief Vérifie si l'adaptateur est initialisé
    */
   bool is_initialized() const { return this->initialized_; }
-  
+
   // ===== Callbacks V4L2 (publics pour esp_video_ops) =====
-  
+
   static esp_err_t v4l2_init(void *video);
   static esp_err_t v4l2_deinit(void *video);
   static esp_err_t v4l2_start(void *video, uint32_t type);
   static esp_err_t v4l2_stop(void *video, uint32_t type);
-  static esp_err_t v4l2_enum_format(void *video, uint32_t type, 
+  static esp_err_t v4l2_enum_format(void *video, uint32_t type,
                                      uint32_t index, uint32_t *pixel_format);
   static esp_err_t v4l2_set_format(void *video, const void *format);
   static esp_err_t v4l2_get_format(void *video, void *format);
@@ -102,11 +105,11 @@ class MipiDsiCamV4L2Adapter {
   static esp_err_t v4l2_qbuf(void *video, void *buffer);
   static esp_err_t v4l2_dqbuf(void *video, void *buffer);
   static esp_err_t v4l2_querycap(void *video, void *cap);
-  
+
  protected:
   MipiCameraV4L2Context context_;
   bool initialized_{false};
-  
+
   // Table des opérations V4L2
   static const esp_video_ops s_video_ops;
 };
@@ -115,4 +118,5 @@ class MipiDsiCamV4L2Adapter {
 } // namespace esphome
 
 #endif // USE_ESP32_VARIANT_ESP32P4
+
 
