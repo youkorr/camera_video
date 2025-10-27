f2e30a2081e3bb5460a4cf9fbb770593b18a4cdf
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-27 02:23:32 +0100

    Create __init__.py

---
+++ components/h264/__init__.py
@@ -0,0 +1,26 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@youkorr"]
+DEPENDENCIES = ["mipi_dsi_cam"]
+
+h264_ns = cg.esphome_ns.namespace("h264")
+H264Encoder = h264_ns.class_("H264Encoder", cg.Component)
+
+CONF_BITRATE = "bitrate"
+CONF_GOP_SIZE = "gop_size"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(H264Encoder),
+    cv.Optional(CONF_BITRATE, default=2000000): cv.int_range(min=100000, max=20000000),
+    cv.Optional(CONF_GOP_SIZE, default=30): cv.int_range(min=1, max=300),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.set_bitrate(config[CONF_BITRATE]))
+    cg.add(var.set_gop_size(config[CONF_GOP_SIZE]))
+    cg.add_define("USE_H264_ENCODER")
+    # ...
