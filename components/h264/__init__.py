74b1ba5cb712f402340141b1bff376db993ce731
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-27 02:44:30 +0100

    Update __init__.py

--- components/h264/__init__.py
+++ components/h264/__init__.py
@@ -8,11 +8,18 @@ DEPENDENCIES = ["mipi_dsi_cam"]
 h264_ns = cg.esphome_ns.namespace("h264")
 H264Encoder = h264_ns.class_("H264Encoder", cg.Component)
 
+# Ajouter la référence à la caméra
+CONF_CAMERA_ID = "camera_id"
 CONF_BITRATE = "bitrate"
 CONF_GOP_SIZE = "gop_size"
 
+# Import du namespace mipi_dsi_cam pour la référence
+mipi_dsi_cam_ns = cg.esphome_ns.namespace("mipi_dsi_cam")
+MipiDsiCam = mipi_dsi_cam_ns.class_("MipiDsiCam")
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(H264Encoder),
+    cv.Required(CONF_CAMERA_ID): cv.use_id(MipiDsiCam),  # ✅ AJOUTER
     cv.Optional(CONF_BITRATE, default=2000000): cv.int_range(min=100000, max=20000000),
     cv.Optional(CONF_GOP_SIZE, default=30): cv.int_range(min=1, max=300),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -20,7 +27,11 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # Lier à la caméra ✅
+    camera = await cg.get_variable(config[CONF_CAMERA_ID])
+    cg.add(var.set_camera(camera))
+    
     cg.add(var.set_bitrate(config[CONF_BITRATE]))
     cg.add(var.set_gop_size(config[CONF_GOP_SIZE]))
     cg.add_define("USE_H264_ENCODER")
-    # ...
