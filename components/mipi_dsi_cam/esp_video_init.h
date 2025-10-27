085d50fdac7d598b09a21d63277be7803bfc83b7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-27 07:13:51 +0100

    Update esp_video_init.h

--- components/mipi_dsi_cam/esp_video_init.h
+++ components/mipi_dsi_cam/esp_video_init.h
@@ -89,6 +89,22 @@ typedef struct esp_video_init_config {
     const esp_video_init_isp_config_t *isp;
 } esp_video_init_config_t;
 
+/** ✅ AJOUT : Structure pour les opérations V4L2 **/
+struct esp_video_ops {
+  esp_err_t (*init)(void *video);
+  esp_err_t (*deinit)(void *video);
+  esp_err_t (*start)(void *video, uint32_t type);
+  esp_err_t (*stop)(void *video, uint32_t type);
+  esp_err_t (*enum_format)(void *video, uint32_t type, uint32_t index, uint32_t *pixel_format);
+  esp_err_t (*set_format)(void *video, const void *format);
+  esp_err_t (*get_format)(void *video, void *format);
+  esp_err_t (*reqbufs)(void *video, void *reqbufs);
+  esp_err_t (*querybuf)(void *video, void *buffer);
+  esp_err_t (*qbuf)(void *video, void *buffer);
+  esp_err_t (*dqbuf)(void *video, void *buffer);
+  esp_err_t (*querycap)(void *video, void *cap);
+};
+
 /**
  * @brief Initialize video hardware and software (I2C, MIPI CSI, etc.)
  */
@@ -98,6 +114,18 @@ esp_err_t esp_video_init(const esp_video_init_config_t *config);
  * @brief Deinitialize and free video hardware resources.
  */
 esp_err_t esp_video_deinit(void);
+
+/**
+ * @brief Register a video device with the VFS system
+ * 
+ * @param device_id Device number (0-3)
+ * @param video_device Pointer to device context
+ * @param user_ctx User context pointer
+ * @param ops Pointer to operations table
+ * @return ESP_OK on success
+ */
+esp_err_t esp_video_register_device(int device_id, void *video_device, 
+                                    void *user_ctx, const void *ops);
 
 #ifdef __cplusplus
 }
