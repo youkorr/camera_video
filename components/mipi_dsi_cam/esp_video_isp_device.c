254148de655a85d06cbe6df280ed44a3bda312c2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-28 07:12:32 +0100

    Update esp_video_isp_device.c

--- components/mipi_dsi_cam/esp_video_isp_device.c
+++ components/mipi_dsi_cam/esp_video_isp_device.c
@@ -19,6 +19,8 @@
 #include "freertos/queue.h"
 
 #include "esp_video.h"
+#include "mipi_dsi_cam.h"
+#include "mipi_dsi_cam_v4l2_adapter.h"
 //#include "esp_video_device.h"
 //#include "esp_video_isp_ioctl.h"
 //#include "esp_video_device_internal.h"
