62e12bd84e21a74dfb9c0860494e305bdcfd4f47
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-29 03:02:40 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -5,21 +5,12 @@
 #include "esphome/components/lvgl/lvgl_esphome.h"
 
 #ifdef USE_ESP32_VARIANT_ESP32P4
-#include <fcntl.h>
-#include "ioctl.h"
-#include "mman.h"
-//#include "../lvgl_camera_display/mman.h"
-#include "../mipi_dsi_cam/videodev2.h"
-#include "../mipi_dsi_cam/esp_video_device.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
 #include "driver/ppa.h"
 #endif
 
 namespace esphome {
 namespace lvgl_camera_display {
-
-// Configuration
-#define VIDEO_BUFFER_COUNT 2
 
 enum RotationAngle {
   ROTATION_0 = 0,
@@ -47,32 +38,16 @@ class LVGLCameraDisplay : public Component {
   mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
   
 #ifdef USE_ESP32_VARIANT_ESP32P4
-  // V4L2 device
-  int video_fd_{-1};
-  const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};
+  // Buffer d'affichage
+  uint8_t *display_buffer_{nullptr};
+  size_t frame_size_{0};
   
-  // Buffers mmappés
-  uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT]{nullptr};
-  size_t buffer_length_{0};
-  int current_buffer_index_{-1};
-
-  uint8_t *work_buffer_{nullptr};
-  
-  // PPA
+  // PPA pour transformations
   ppa_client_handle_t ppa_handle_{nullptr};
   uint8_t *transform_buffer_{nullptr};
   size_t transform_buffer_size_{0};
   
-  // Méthodes V4L2
-  bool open_v4l2_device_();
-  bool setup_v4l2_format_();
-  bool setup_v4l2_buffers_();
-  bool start_v4l2_streaming_();
-  bool capture_v4l2_frame_(uint8_t **frame_data);
-  void release_v4l2_frame_();
-  void cleanup_v4l2_();
-  
-  // PPA
+  // Méthodes PPA
   bool init_ppa_();
   void deinit_ppa_();
   bool transform_frame_(const uint8_t *src, uint8_t *dst);
@@ -89,10 +64,10 @@ class LVGLCameraDisplay : public Component {
   
   uint32_t frame_count_{0};
   uint32_t drop_count_{0};
+  uint32_t last_frame_sequence_{0};
   uint32_t last_update_time_{0};
   uint32_t last_fps_time_{0};
   bool first_update_{true};
-  bool v4l2_streaming_{false};
 };
 
 }  // namespace lvgl_camera_display
