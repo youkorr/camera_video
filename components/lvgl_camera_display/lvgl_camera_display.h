9c5a5d3ed2131a258300cd05e2f8608485edf1d2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-01 03:22:56 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -2,25 +2,23 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "lvgl.h"
 #include "esphome/components/lvgl/lvgl_esphome.h"
 
 #ifdef USE_ESP32_VARIANT_ESP32P4
 #include <fcntl.h>
 #include "ioctl.h"
 #include "mman.h"
+#include "../lvgl_camera_display/mman.h"
 #include "../mipi_dsi_cam/videodev2.h"
 #include "../mipi_dsi_cam/esp_video_device.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
 #include "driver/ppa.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
 #endif
 
 namespace esphome {
 namespace lvgl_camera_display {
 
+// Configuration
 #define VIDEO_BUFFER_COUNT 2
 
 enum RotationAngle {
@@ -44,40 +42,26 @@ class LVGLCameraDisplay : public Component {
   void set_rotation(RotationAngle rotation) { this->rotation_ = rotation; }
   void set_mirror_x(bool enable) { this->mirror_x_ = enable; }
   void set_mirror_y(bool enable) { this->mirror_y_ = enable; }
-  
-  void set_direct_mode(bool enable) { this->direct_mode_ = enable; }
-  void set_use_ppa(bool enable) { this->use_ppa_ = enable; }
-
-  void stop_capture();
 
  protected:
   mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
   
 #ifdef USE_ESP32_VARIANT_ESP32P4
+  // V4L2 device
   int video_fd_{-1};
   const char *video_device_{ESP_VIDEO_MIPI_CSI_DEVICE_NAME};
   
+  // Buffers mmappés
   uint8_t *mmap_buffers_[VIDEO_BUFFER_COUNT]{nullptr};
   size_t buffer_length_{0};
   int current_buffer_index_{-1};
-  uint8_t *work_buffer_{nullptr};
   
+  // PPA
   ppa_client_handle_t ppa_handle_{nullptr};
   uint8_t *transform_buffer_{nullptr};
   size_t transform_buffer_size_{0};
   
-  lv_disp_t *lvgl_display_{nullptr};
-  lv_disp_draw_buf_t *lvgl_draw_buf_{nullptr};
-  uint8_t *lvgl_framebuffer_{nullptr};
-  size_t lvgl_framebuffer_size_{0};
-  
-  uint8_t *aligned_buffer_{nullptr};
-  size_t aligned_buffer_size_{0};
-  
-  TaskHandle_t capture_task_handle_{nullptr};
-  volatile bool task_running_{false};
-  SemaphoreHandle_t lvgl_mutex_{nullptr};
-  
+  // Méthodes V4L2
   bool open_v4l2_device_();
   bool setup_v4l2_format_();
   bool setup_v4l2_buffers_();
@@ -86,14 +70,10 @@ class LVGLCameraDisplay : public Component {
   void release_v4l2_frame_();
   void cleanup_v4l2_();
   
+  // PPA
   bool init_ppa_();
   void deinit_ppa_();
   bool transform_frame_(const uint8_t *src, uint8_t *dst);
-  
-  bool init_direct_mode_();
-  
-  static void capture_task_(void *param);
-  void capture_loop_();
 #endif
 
   lv_obj_t *canvas_obj_{nullptr};
@@ -103,17 +83,25 @@ class LVGLCameraDisplay : public Component {
   RotationAngle rotation_{ROTATION_0};
   bool mirror_x_{false};
   bool mirror_y_{false};
-  bool direct_mode_{false};
-  bool use_ppa_{true};
   uint32_t update_interval_{33};
   
   uint32_t frame_count_{0};
   uint32_t drop_count_{0};
+  uint32_t last_update_time_{0};
+  uint32_t last_fps_time_{0};
+  bool first_update_{true};
   bool v4l2_streaming_{false};
 };
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
+
+
+
+
+
+
+
 
 
 
